efc38e56609e030ed8f2305ce35c5e5bc436b6da
Long Zer <gialongminecraft@gmail.com>
2023-01-08 13:19:56 +0700

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,48 @@
+<script setup>
+import WelcomeItem from "./WelcomeItem.vue";
+import DocumentationIcon from "./icons/IconDocumentation.vue";
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from "./icons/IconEcosystem.vue";
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from "./icons/IconSupport.vue";
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon/>
+    </template>
+    <template #heading>Lưu ý</template>
+    <text>Lưu ý: Đổi tên trước khi nộp theo định dạng &lt;tên&gt;.vrblocks hoặc &lt;tên1&gt;,&lt;tên2&gt;,&lt;tên3&gt;.vrblocks</text>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Cách nộp bài</template>
+    
+    <div>Nộp bài qua <a
+      href="https://classroom.google.com/c/NTczNDA4MDk4ODcy?cjc=5wd7u2z"
+      target="_blank"
+      rel="noopener"
+      >Google Classroom</a
+    >.</div>
+    <text>
+      <a
+        href="https://classroom.google.com/c/NTczNDA4MDk4ODcy?cjc=5wd7u2z"
+      target="_blank"
+      rel="noopener"
+      >Video hướng dẫn</a
+    > 
+    </text>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Documentation</template>
+      
+  </WelcomeItem>
+</template>
